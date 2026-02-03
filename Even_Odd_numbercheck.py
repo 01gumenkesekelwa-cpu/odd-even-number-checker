b5c318b127a161c7eb74b21bbc0a9c2e0d77bc8f
Sekelwa Gumenke <01gumenkesekelwa@gmail.com>
2026-02-03 19:29:05 +0200

    Add files via upload

---
+++ Even_Odd_numbercheck.py
@@ -0,0 +1,12 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> Number = int(input(print("Enter a number")))
+Enter a number
+None12
+>>> if Number %2 ==0:
+...     print("even")
+... else:
+...     print("Odd")
+... 
+...     
+even
